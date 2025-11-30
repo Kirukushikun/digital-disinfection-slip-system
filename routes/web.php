6ffc577995a90c5e5dd0e194e445ae8e7ec153cf
raniel-roque <raniel.roque0509@gmail.com>
2025-11-30 19:16:18 +0800

    Added static dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -12,11 +13,10 @@ Route::post('/login', [SessionController::class,'store'])->name('login.store');
 Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 
 Route::middleware(['auth', 'user.type:0'])->prefix('user')->name('user.')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'user'])->name('dashboard');
-
-    Route::view('/incoming-trucks', 'user.incoming-trucks')->name('incoming-trucks');
-    Route::view('/outgoing-trucks', 'user.outgoing-trucks')->name('outgoing-trucks');
-    Route::view('/completed-trucks', 'user.completed-trucks')->name('completed-trucks');
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+    Route::get('/incoming-trucks', [UserController::class, 'incomingTrucks'])->name('incoming-trucks');
+    Route::get('/outgoing-trucks', [UserController::class, 'outgoingTrucks'])->name('outgoing-trucks');
+    Route::get('/completed-trucks', [UserController::class, 'completedTrucks'])->name('completed-trucks');
 });
 
 Route::middleware(['auth', 'user.type:1'])->prefix('admin')->name('admin.')->group(function () {
