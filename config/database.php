964466b644707dafb64f0a2f48eac13380a098ff
Iverson Craig <148548092+Kirukushikun@users.noreply.github.com>
2026-02-03 14:33:03 +0800

    Spatie x Google Drive Backup

--- config/database.php
+++ config/database.php
@@ -61,6 +61,12 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'dump' => [
+                'dump_binary_path' => 'C:\laragon\bin\mysql\mysql-8.0.30-winx64\bin', // adjust this
+                'use_single_transaction' => true,
+                'timeout' => 60,
+                'add_extra_option' => '--no-tablespaces', // handles permission issues
+            ],
         ],
 
         'mariadb' => [
